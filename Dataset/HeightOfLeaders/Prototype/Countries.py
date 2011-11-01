b2c4c44614c916f49f29126c57d2dcc54b2781f3
Noah Hall <eeue56@bangor.ac.uk>
2011-11-01 12:57:04 +0000

    Some more restructoring and moving, no code changes

---
+++ Dataset/HeightOfLeaders/Prototype/Countries.py
@@ -0,0 +1,85 @@
+class Country(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.members = dict()
+
+    def __str__(self):
+        return repr(self.name)
+
+    def __repr__(self):
+        return repr(self.members.values())
+    
+    def add_member(self, name, height_in_cm):
+        if name in self.members:
+            raise ValueError('A member with this name already exists!')
+        self.members[name] = Member(name, height_in_cm)
+
+    def add_members(self, members):
+        for member in members:
+            self.add_member(*member)
+
+    def remove_member(self, name):
+        if name not in self.members:
+            raise ValueError('No member with this name exists!')
+        del self.members[name]
+
+    def remove_all_members(self):
+        del self.members
+        self.members = dict()
+
+    def _verify_at_least_1_member(self):
+        if self.number_of_members() == 0:
+            raise ValueError('Must have at least one member!')
+
+    def number_of_members(self):
+        return len(self.members)
+
+    def tallest_height(self):
+        self._verify_at_least_1_member()
+        return max(member.height for member in self.members.values())
+
+    def shortest_height(self):
+        self._verify_at_least_1_member()
+        return min(member.height for member in self.members.values())
+
+    def shortest_member(self):
+        self._verify_at_least_1_member()
+        shortest_member = sorted(self.members.iteritems(), key=lambda (k, v): (v, k))[0]
+        return shortest_member[0]
+
+    def tallest_member(self):
+        self._verify_at_least_1_member()
+        tallest_member = sorted(self.members.iteritems(), key=lambda (k, v): (v, k))[-1]
+        return tallest_member[0]
+
+    def average_height(self):
+        self._verify_at_least_1_member()
+        return sum(member.height for member in self.members.values())/float(self.number_of_members())
+
+
+class Member(object):
+
+    def __init__(self, name, height_in_cm):
+        if name == '':
+            raise ValueError("Name must have a value!")
+        self.name = name
+        self.height = float(height_in_cm)
+
+    def __str__(self):
+        return repr(self.name)
+
+    def __repr__(self):
+        return repr('{name} : {height}'.format(name=self.name, height=self.height))
+
+
+def test():
+    country = Country('UK')
+    country.add_members([('George',129),('Harry',140)])
+    country.add_member('Dick', 143)
+    print repr(country)
+    country.remove_all_members()
+    print repr(country)
+    
+if __name__ == '__main__':
+    test()
---
+++ Dataset/HeightOfLeaders/Prototype/height_of_leaders.py
@@ -0,0 +1,31 @@
+from Countries import Country
+
+def read_leaders(filename):
+    
+    countries = dict()
+    
+    with open(filename, 'r') as f:
+        for line in f.readlines()[1:]:
+            name, height, rubbish , country = [piece.strip() for piece in line.split(',')]
+            if country not in countries:
+                countries[country] = Country(country)
+            countries[country].add_member(name, height)
+            
+    return countries
+                
+                
+def main(filename):
+    
+    countries = read_leaders(filename)
+    for country in countries.values():
+        print country.name            
+        print 'Number of members: {}'.format(country.number_of_members())
+        print 'The tallest member is {}, who is {}cm'.format(country.tallest_member(),country.tallest_height())
+        print 'The shortest member is {}, who is {}cm'.format(country.shortest_member(),country.shortest_height())
+        print 'The average height is {:2g}cm'.format(country.average_height())
+        print '\n\n'
+       
+
+
+if __name__ == '__main__':
+    main("../Data/HEIGHTS OF LEADERS.csv")
