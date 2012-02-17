05b57edc0bc4786158c43047b3069c1120865b8f
Noah Hall <eeue56@bangor.ac.uk>
2012-02-17 17:29:17 +0000

    added TODO

--- Dataset/Data/WordRelationships/relationships.py
+++ Dataset/Data/WordRelationships/relationships.py
@@ -220,5 +220,5 @@ if __name__ == '__main__':
     with open('pg42.txt') as f:
         other_data = [line for line in f if len(line.strip()) > 1]
         data = map_words(other_data, data)
-
+        
     print make_sentence(data)
